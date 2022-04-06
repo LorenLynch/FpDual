978596f3e8d18c4f1c4913645f17802d3d147c24
Lynch McRae <loren.lynch.mcrae@accenture.com>
2022-04-06 13:51:08 +0200

    Initial commit

---
+++ src/edu/fpdual/ejemplo/project/Main.java
@@ -0,0 +1,16 @@
+package edu.fpdual.ejemplo.project;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Main main = new Main();
+        main.printMessage();
+    }
+
+    public void printMessage(){
+        int variableInt = 5;
+        variableInt = 5;
+        variableInt = 7;
+        System.out.println(variableInt);
+    }
+}
